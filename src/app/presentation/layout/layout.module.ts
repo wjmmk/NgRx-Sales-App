922b148ccd181af07cb7315a69d2307d892d0a9a
William Mosquera <wjmmk3@gmail.com>
2023-03-02 15:43:46 -0500

    Presentation Module Created

---
+++ src/app/presentation/layout/layout.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { BaseLoggedComponent } from './base-logged/base-logged.component';
+import { BaseAuthComponent } from './base-auth/base-auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent,
+    CardComponent,
+    NavbarComponent,
+    LogoComponent,
+    BaseLoggedComponent,
+    BaseAuthComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
