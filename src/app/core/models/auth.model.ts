065e0a9371665d8b167001c817227675bfa01331
William Mosquera <wjmmk3@gmail.com>
2023-03-06 08:08:56 -0500

    Interface added

---
+++ src/app/core/models/auth.model.ts
@@ -0,0 +1,21 @@
+
+export interface ILogin {
+    identifier: string;
+    password: string;
+}
+
+export interface IUser {
+     id: number;
+     username: string;
+     email: string;
+     provider: string;
+     confirmed: boolean;
+     blocked?: boolean;
+     created_at: Date;
+     update_at: Date;
+} 
+
+export interface IUserResponse {
+    jwt: string;
+    user: IUser;
+}
