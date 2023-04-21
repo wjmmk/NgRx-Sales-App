3092072734667d597f609045bb7b17aa752bcb75
William Mosquera <wjmmk3@gmail.com>
2023-04-21 15:25:46 -0500

    app-routing.module.ts refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,19 @@ import { BaseAuthComponent } from './presentation/layout/base-auth/base-auth.com
 import { BaseLoggedComponent } from './presentation/layout/base-logged/base-logged.component';
 
 const routes: Routes = [
-  {path: '', component: BaseLoggedComponent, canActivate: [LoggedGuard], children: [
-    { path: 'home', loadChildren: () => import('./presentation/features/home/home.module').then( (m) => m.HomeModule)}
-  ]},
-  {path: 'auth', component: BaseAuthComponent, canActivate: [UnloggedGuard], children: [
-    { path: 'login', loadChildren: () => import('./presentation/features/auth/login/login.module').then( (m) => m.LoginModule)},
-    { path: 'signup', loadChildren: () => import('./presentation/features/auth/signup/signup.module').then( (m) => m.SignupModule)}
-  ]}
+  {
+    path: '', component: BaseLoggedComponent, canActivate: [LoggedGuard],
+    children: [
+      { path: 'home', loadChildren: () => import('./presentation/features/home/home.module').then((m) => m.HomeModule) }
+    ]
+  },
+  {
+    path: 'auth', component: BaseAuthComponent, canActivate: [UnloggedGuard],
+    children: [
+      { path: 'login', loadChildren: () => import('./presentation/features/auth/login/login.module').then((m) => m.LoginModule) },
+      { path: 'signup', loadChildren: () => import('./presentation/features/auth/signup/signup.module').then((m) => m.SignupModule) }
+    ]
+  }
 ];
 
 @NgModule({
